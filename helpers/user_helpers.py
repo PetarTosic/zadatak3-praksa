bd74fb19299a56578f2af7d675fdb4f0225e65cf
PetarTosic <petar94bs@gmail.com>
2023-07-18 13:26:33 +0200

    Added more typehinting

--- helpers/user_helpers.py
+++ helpers/user_helpers.py
@@ -1,43 +1,47 @@
-def addTruck(driver: dict, truck: dict):
+from model.UserModel import TruckDriver, Dispatcher, Manager
+from model.UserBaseModel import User
+from model.VehicleModel import Truck
+
+def addTruck(driver: TruckDriver, truck: Truck):
     driver.truck = truck
     truck.truckDriver = driver
 
-def addManager(user: dict, mng: str):
+def addManager(user: User, mng: str):
     user.manager = mng
 
-def setArea(user: dict, area: str):
+def setArea(user: User, area: str):
     user.area = area
 
-def addDispatcher(driver: dict, disp: dict):
+def addDispatcher(driver: TruckDriver, disp: Dispatcher):
     driver.dispatcher = disp
 
-def addLoadAndArea(driver: dict, load: str, area: str):
-    if driver.truck != {} and driver.truck.trailer != {}:
+def addLoadAndArea(driver: TruckDriver, load: str, area: str):
+    if not driver.truck and not driver.truck.trailer:
       driver.load = load
       driver.area = area
     else:
       print('Driver doesn\'t have truck and/or trailer.')
 
-def addTruckDriver(user: dict, driver: dict):
+def addTruckDriver(user: User, driver: TruckDriver):
     user.truckDrivers.append(driver)
     if(type(user) == Dispatcher):  
       driver.dispatcher = user
     else:
       driver.manager = user.jmbg
 
-def giveDriverJob(disp: dict, load: str, driver: dict):
+def giveDriverJob(disp: Dispatcher, load: str, driver: dict):
     if driver not in disp.truckDrivers:
       disp.truckDrivers.append(driver)
     addLoadAndArea(driver, load, disp.area)
 
-def showTruckDrivers(user: dict):
+def showTruckDrivers(user: User):
     if len(user.truckDrivers) > 0:
       for driver in user.truckDrivers:
         print(driver)
     else:
       print('No drivers assigned.')
 
-def addDispatcher(manager: dict, disp: dict):
+def addDispatcher(manager: Manager, disp: Dispatcher):
     manager.dispatchers.append(disp)
     addManager(disp, manager)
     if manager.area:
--- helpers/vehicle_helpers.py
+++ helpers/vehicle_helpers.py
@@ -1,18 +1,19 @@
-from model.VehicleBaseModel import Repair
+from model.VehicleBaseModel import Repair, Vehicle
+from model.VehicleModel import Truck, Trailer
 
-def addTruckDriver(truck: dict, driver: dict):
+def addTruckDriver(truck: Truck, driver: Trailer):
     truck.truckDriver = driver
 
-def addTrailer(truck: dict, trailer: dict):
+def addTrailer(truck: Truck, trailer: Trailer):
     truck.trailer = trailer
     addTruck(trailer, truck)
 
-def addTruck(trailer: dict , truck: dict):
+def addTruck(trailer: Trailer , truck: Truck):
     trailer.truck = truck
 
-def addRepair(vehicle: dict, date: str, description: str):
+def addRepair(vehicle: Vehicle, date: str, description: str):
     vehicle.repairs.append(Repair(date, description, vehicle))
 
-def showRepairs(vehicle: dict):
+def showRepairs(vehicle: Vehicle):
     for repair in vehicle.repairs:
       print(repair)
